1d1afd1f63d6d314d69ba66010b5348486d65a48
Skanda1101 <r.skanda1101@gmail.com>
2024-05-20 11:46:40 +0530

    Updated code in GelAllLearner and GetLearnerById

--- Hydra.BusinessLayer/Concrete/Service/LearnerService/LearnerManagmentService.cs
+++ Hydra.BusinessLayer/Concrete/Service/LearnerService/LearnerManagmentService.cs
@@ -178,7 +178,12 @@ namespace Hydra.BusinessLayer.Repository.Service.LearnerService
                                                     {
                                                         UserId = s.Id,
                                                         Name = $"{s.FirstName} {s.LastName}",
+                                                        FirstName = s.FirstName,
+                                                        LastName = s.LastName,
                                                         Email = s.Email,
+                                                        Email2 = s.Email2,
+                                                        Email3 = s.Email3,
+                                                        MobileNumber = s.MobileNumber,
                                                         LearnerBadgeModel = s.LearnerBadge.Where(x => x.IsActive && x.IsRevoked == false).Select(s => new LearnerBadgeModel
                                                         {
                                                             BadgeId = s.BadgeId,
@@ -223,6 +228,10 @@ namespace Hydra.BusinessLayer.Repository.Service.LearnerService
                                                                             FirstName = s.FirstName,
                                                                             LastName = s.LastName,
                                                                             Email = s.Email,
+                                                                            Email = s.Email,
+                                                                            Email2 = s.Email2,
+                                                                            Email3 = s.Email3,
+                                                                            MobileNumber = s.MobileNumber,
                                                                             LearnerBadgeModel = s.LearnerBadge.Where(x => x.IsActive && x.IsRevoked == false).Select(s => new LearnerBadgeModel
                                                                             {
                                                                                 BadgeId = s.BadgeId,
